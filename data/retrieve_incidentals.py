4dd72bb5ce45fc92eb577f6734a607641cbaba88
Philip Brohan <philip@brohan.org>
2019-11-11 09:53:11 +0000

    Added script to retrieve land mask for ploting.

---
+++ data/retrieve_incidentals.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# Download everything we need except the 20CR data.
+# Store it on $SCRATCH
+
+import os
+import urllib.request
+
+# Land mask for plots
+
+srce=("https://s3-eu-west-1.amazonaws.com/"+
+      "philip.brohan.org.ml-gcm/opfc_global_2019.nc")
+dst="%s/fixed_fields/land_mask/opfc_global_2019.nc" % os.getenv('SCRATCH')
+if not os.path.exists(dst):
+    if not os.path.isdir(os.path.dirname(dst)):
+        os.makedirs(os.path.dirname(dst))
+    urllib.request.urlretrieve(srce, dst)
