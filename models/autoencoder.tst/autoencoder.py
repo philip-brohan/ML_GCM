2fea4391ed3dfcbed9b9408e36ebac9b016fa5e5
Philip Brohan <philip@brohan.org>
2019-11-13 17:48:43 +0000

    Added predictor and test autoencoder model.

---
+++ models/autoencoder.tst/autoencoder.py
@@ -0,0 +1,190 @@
+#!/usr/bin/env python
+
+# Convolutional autoencoder for 20CRv2c fields.
+
+import os
+import sys
+import tensorflow as tf
+#tf.enable_eager_execution()
+from tensorflow.data import Dataset
+import pickle
+import numpy
+from glob import glob
+
+# How many epochs to train for
+n_epochs=26
+
+# How big a latent space
+latent_dim=100
+
+# Target data setup
+buffer_size=100
+batch_size=1
+
+# Training datasets with four variables
+input_file_dir=(("%s/ML_GCM/datasets/" +
+                "20CR2c/air.2m/training/") %
+                   os.getenv('SCRATCH'))
+t2m_files=glob("%s/*.tfd" % input_file_dir)
+n_steps=len(t2m_files)
+tr_tfd = tf.constant(t2m_files)
+
+# Create TensorFlow Dataset object from the file names
+tr_data = Dataset.from_tensor_slices(tr_tfd).repeat(n_epochs)
+
+# Make a 5-variable tensor including the insolation field
+def load_tensor(file_name):
+    sict  = tf.read_file(file_name)
+    t2m = tf.parse_tensor(sict,numpy.float32)
+    t2m = tf.reshape(t2m,[79,159,1])
+    file_name = tf.strings.regex_replace(file_name,
+                                      'air.2m','prmsl')
+    sict  = tf.read_file(file_name)
+    prmsl = tf.parse_tensor(sict,numpy.float32)
+    prmsl = tf.reshape(prmsl,[79,159,1])
+    file_name = tf.strings.regex_replace(file_name,
+                                      'prmsl','uwnd.10m')
+    sict  = tf.read_file(file_name)
+    uwnd  = tf.parse_tensor(sict,numpy.float32)
+    uwnd  = tf.reshape(uwnd,[79,159,1])
+    file_name = tf.strings.regex_replace(file_name,
+                                      'uwnd.10m','vwnd.10m')
+    sict  = tf.read_file(file_name)
+    vwnd  = tf.parse_tensor(sict,numpy.float32)
+    vwnd  = tf.reshape(vwnd,[79,159,1])
+    file_name = tf.strings.regex_replace(file_name,
+                                      'vwnd.10m','insolation')
+    fdte = tf.strings.substr(file_name,-17,17)
+    mnth = tf.strings.substr(fdte,5,2)
+    dy   = tf.strings.substr(fdte,8,2)
+    dy = tf.cond(tf.math.equal(mnth+dy,'0229'),
+                               lambda: tf.constant('28'),lambda: dy)
+    file_name=(tf.strings.substr(file_name,0,tf.strings.length(file_name)-17)+
+                '1969-'+mnth+'-'+dy+tf.strings.substr(fdte,tf.strings.length(fdte)-7,7))
+    sict  = tf.read_file(file_name)
+    insol = tf.parse_tensor(sict,numpy.float32)
+    insol = tf.reshape(insol,[79,159,1])
+    ict = tf.concat([t2m,prmsl,uwnd,vwnd,insol],2) # Now [79,159,5]
+    ict = tf.reshape(ict,[79,159,5])
+    return ict
+
+
+tr_target = tr_data.map(load_tensor)
+tr_source = tr_data.map(load_tensor)
+
+tr_data = Dataset.zip((tr_source, tr_target))
+tr_data = tr_data.shuffle(buffer_size).batch(batch_size)
+
+# Same for the test dataset
+input_file_dir=(("%s/ML_GCM/datasets/" +
+                "20CR2c/air.2m/test/") %
+                   os.getenv('SCRATCH'))
+t2m_files=glob("%s/*.tfd" % input_file_dir)
+test_steps=len(t2m_files)
+test_tfd = tf.constant(t2m_files)
+test_data = Dataset.from_tensor_slices(test_tfd).repeat(n_epochs)
+test_target = test_data.map(load_tensor)
+test_source = test_data.map(load_tensor)
+test_data = Dataset.zip((test_source, test_target))
+test_data = test_data.batch(batch_size)
+
+# Add noise to latent vector
+def noise(encoded):
+    epsilon = tf.keras.backend.random_normal(tf.keras.backend.shape(encoded),
+                                             mean=0.0,stddev=0.5)
+    return encoded+epsilon
+# Normalise the latent vector
+def stdise(encoded):
+    encoded = encoded-tf.keras.backend.mean(encoded)
+    encoded = encoded/tf.keras.backend.std(encoded)
+    return encoded
+
+# Input placeholder
+original = tf.keras.layers.Input(shape=(79,159,5,), name='encoder_input')
+# Encoding layers
+x = tf.keras.layers.Conv2D(5, (3, 3), padding='same')(original)
+x = tf.keras.layers.ELU()(x)
+x = tf.keras.layers.Dropout(0.3)(x)
+x = tf.keras.layers.Conv2D(10, (3, 3), strides= (2,2), padding='valid')(x)
+x = tf.keras.layers.ELU()(x)
+x = tf.keras.layers.Dropout(0.3)(x)
+x = tf.keras.layers.Conv2D(30, (3, 3), strides= (2,2), padding='valid')(x)
+x = tf.keras.layers.ELU()(x)
+x = tf.keras.layers.Dropout(0.3)(x)
+x = tf.keras.layers.Conv2D(90, (3, 3), strides= (2,2), padding='valid')(x)
+x = tf.keras.layers.ELU()(x)
+x = tf.keras.layers.Dropout(0.3)(x)
+# N-dimensional latent space representation
+x = tf.keras.layers.Reshape(target_shape=(9*19*90,))(x)
+x = tf.keras.layers.Dense(latent_dim,)(x)
+encoded  = tf.keras.layers.Lambda(stdise, output_shape=(latent_dim,))(x)
+
+# Define an encoder model
+encoder = tf.keras.models.Model(original, encoded, name='encoder')
+
+# Add a noise layer to make the encoder variational
+noise_i = tf.keras.layers.Input(shape=(latent_dim,), name='noise_input')
+noise_o  = tf.keras.layers.Lambda(noise, output_shape=(latent_dim,))(noise_i)
+noise_m = tf.keras.models.Model(noise_i, noise_o, name='noise_m')
+
+# Decoding layers
+encoded = tf.keras.layers.Input(shape=(latent_dim,), name='decoder_input')
+x = tf.keras.layers.Dense(9*19*90,)(encoded)
+x = tf.keras.layers.Reshape(target_shape=(9,19,90,))(x)
+x = tf.keras.layers.Conv2DTranspose(90, (3, 3),  strides= (2,2), padding='valid')(x)
+x = tf.keras.layers.ELU()(x)
+x = tf.keras.layers.Conv2DTranspose(30, (3, 3),  strides= (2,2), padding='valid')(x)
+x = tf.keras.layers.ELU()(x)
+x = tf.keras.layers.Conv2DTranspose(10, (3, 3),  strides= (2,2), padding='valid')(x)
+x = tf.keras.layers.ELU()(x)
+decoded = tf.keras.layers.Conv2D(5, (3, 3), padding='same')(x) # Will be 75x159x5 
+
+# Define a generator (decoder) model
+generator = tf.keras.models.Model(encoded, decoded, name='generator')
+
+# Combine the encoder and the generator into an autoencoder
+output = generator(encoder(original))
+autoencoder = tf.keras.models.Model(inputs=original, outputs=output, name='autoencoder')
+
+# Combine the encoder, the noise, and the generator, into a variational autoencoder
+output_v = generator(noise_m(encoder(original)))
+var_autoencoder = tf.keras.models.Model(inputs=original, outputs=output_v, name='var_autoencoder')
+
+var_autoencoder.compile(optimizer='adadelta',loss='mean_squared_error')
+
+# Save model and history state after every epoch
+history={}
+history['loss']=[]
+history['val_loss']=[]
+class CustomSaver(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        save_dir=("%s/ML_GCM/autoencoder.tst/"+
+                   "Epoch_%04d") % (
+                         os.getenv('SCRATCH'),epoch)
+        if not os.path.isdir(os.path.dirname(save_dir)):
+            os.makedirs(os.path.dirname(save_dir))
+        for model in ['var_autoencoder','autoencoder','encoder','generator']:
+            if not os.path.isdir(os.path.dirname("%s/%s" % (save_dir,model))):
+                os.makedirs(os.path.dirname("%s/%s" % (save_dir,model)))
+        tf.keras.models.save_model(var_autoencoder,"%s/var_autoencoder" % save_dir)
+        tf.keras.models.save_model(autoencoder,"%s/autoencoder" % save_dir)
+        tf.keras.models.save_model(encoder,"%s/encoder" % save_dir)
+        tf.keras.models.save_model(generator,"%s/generator" % save_dir)
+        history['loss'].append(logs['loss'])
+        history['val_loss'].append(logs['val_loss'])
+        history_file=("%s/ML_GCM/autoencoder.tst/"+
+                      "history_to_%04d.pkl") % (
+                         os.getenv('SCRATCH'),epoch)
+        pickle.dump(history, open(history_file, "wb"))
+        
+saver = CustomSaver()
+
+# Train the autoencoder
+history=var_autoencoder.fit(x=tr_data,
+                            epochs=n_epochs,
+                            steps_per_epoch=n_steps,
+                            validation_data=test_data,
+                            validation_steps=test_steps,
+                            verbose=1,
+                            callbacks=[saver])
+
---
+++ models/autoencoder.tst/plot_random_latent_space.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+
+# Atmospheric state - near-surface temperature, u-wind, v-wind, and prmsl.
+# Show the field associated with a random latent space state.
+
+import os
+import sys
+import IRData.twcr as twcr
+
+import tensorflow as tf
+tf.enable_eager_execution()
+
+import iris
+import numpy
+import math
+
+import matplotlib
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+from matplotlib.patches import Rectangle
+from matplotlib.lines import Line2D
+
+from pandas import qcut
+
+sys.path.append('%s/../../lib/' % os.path.dirname(__file__))
+from normalise import unnormalise_t2m
+from normalise import unnormalise_prmsl
+from normalise import unnormalise_wind
+
+from plots import plot_cube
+from plots import wind_field
+from plots import quantile_normalise_t2m
+from plots import draw_lat_lon
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("--epoch", help="Epoch",
+                    type=int,required=False,default=10)
+
+args = parser.parse_args()
+
+# Define a dummy cube to load with the compressed data
+def dummy_cube():
+    cs=iris.coord_systems.RotatedGeogCS(90,180,0)
+    # Latitudes cover -90 to 90 with 79 values
+    lat_values=numpy.arange(-90,91,180/78)
+    latitude = iris.coords.DimCoord(lat_values,
+                                    standard_name='latitude',
+                                    units='degrees_north',
+                                    coord_system=cs)
+    # Longitudes cover -180 to 180 with 159 values
+    lon_values=numpy.arange(-180,181,360/158)
+    longitude = iris.coords.DimCoord(lon_values,
+                                     standard_name='longitude',
+                                     units='degrees_east',
+                                     coord_system=cs)
+    dummy_data = numpy.zeros((len(lat_values), len(lon_values)))
+    dummy_cube = iris.cube.Cube(dummy_data,
+                               dim_coords_and_dims=[(latitude, 0),
+                                                    (longitude, 1)])
+    return(dummy_cube)
+
+
+# Load the latent-space representation, and convert it back into normal space
+model_save_file=("%s/ML_GCM/autoencoder.tst/"+
+                  "Epoch_%04d/generator") % (
+                      os.getenv('SCRATCH'),args.epoch)
+generator=tf.keras.models.load_model(model_save_file,compile=False)
+
+# Random latent state
+ls=tf.convert_to_tensor(numpy.random.normal(size=100),numpy.float32)
+ls = tf.reshape(ls,[1,100])
+result=generator.predict_on_batch(ls)
+result = tf.reshape(result,[79,159,5])
+t2m=dummy_cube()
+t2m.data = tf.reshape(result.numpy()[:,:,0],[79,159]).numpy()
+t2m.data = unnormalise_t2m(t2m.data)
+prmsl=dummy_cube()
+prmsl.data = tf.reshape(result.numpy()[:,:,1],[79,159]).numpy()
+prmsl.data = unnormalise_prmsl(prmsl.data)
+u10m=dummy_cube()
+u10m.data = tf.reshape(result.numpy()[:,:,2],[79,159]).numpy()
+u10m.data = unnormalise_wind(u10m.data)
+v10m=dummy_cube()
+v10m.data = tf.reshape(result.numpy()[:,:,3],[79,159]).numpy()
+v10m.data = unnormalise_wind(v10m.data)
+    
+mask=iris.load_cube("%s/fixed_fields/land_mask/opfc_global_2019.nc" % os.getenv('SCRATCH'))
+
+# Define the figure (page size, background color, resolution, ...
+fig=Figure(figsize=(19.2,10.8),              # Width, Height (inches)
+           dpi=100,
+           facecolor=(0.5,0.5,0.5,1),
+           edgecolor=None,
+           linewidth=0.0,
+           frameon=False,                # Don't draw a frame
+           subplotpars=None,
+           tight_layout=None)
+fig.set_frameon(False) 
+# Attach a canvas
+canvas=FigureCanvas(fig)
+
+wind_pc=plot_cube(0.2,-180,180,-90,90)   
+cs=iris.coord_systems.RotatedGeogCS(90,180,0)
+rw=iris.analysis.cartography.rotate_winds(u10m,v10m,cs)
+u10m = rw[0].regrid(wind_pc,iris.analysis.Linear())
+v10m = rw[1].regrid(wind_pc,iris.analysis.Linear())
+z=mask.regrid(u10m,iris.analysis.Linear())
+(width,height)=z.data.shape
+z.data=numpy.random.rand(width,height)
+wind_noise_field=wind_field(u10m,v10m,z,sequence=None,epsilon=0.01)
+
+# Define an axes to contain the plot. In this case our axes covers
+#  the whole figure
+ax = fig.add_axes([0,0,1,1])
+ax.set_axis_off() # Don't want surrounding x and y axis
+
+# Lat and lon range (in rotated-pole coordinates) for plot
+ax.set_xlim(-180,180)
+ax.set_ylim(-90,90)
+ax.set_aspect('auto')
+
+# Background
+ax.add_patch(Rectangle((0,0),1,1,facecolor=(0.6,0.6,0.6,1),fill=True,zorder=1))
+
+draw_lat_lon(ax)
+
+# Plot the land mask
+mask_pc=plot_cube(0.05,-180,180,-90,90)   
+mask = mask.regrid(mask_pc,iris.analysis.Linear())
+lats = mask.coord('latitude').points
+lons = mask.coord('longitude').points
+mask_img = ax.pcolorfast(lons, lats, mask.data,
+                         cmap=matplotlib.colors.ListedColormap(
+                                ((0.4,0.4,0.4,0),
+                                 (0.4,0.4,0.4,1))),
+                         vmin=0,
+                         vmax=1,
+                         alpha=1.0,
+                         zorder=20)
+
+
+# Plot the T2M
+t2m_pc=plot_cube(0.05,-180,180,
+                      -90,90)   
+t2m = t2m.regrid(t2m_pc,iris.analysis.Linear())
+t2m=quantile_normalise_t2m(t2m)
+# Adjust to show the wind
+wscale=200
+s=wind_noise_field.data.shape
+wind_noise_field.data=qcut(wind_noise_field.data.flatten(),wscale,labels=False,
+                             duplicates='drop').reshape(s)-(wscale-1)/2
+
+# Plot as a colour map
+wnf=wind_noise_field.regrid(t2m,iris.analysis.Linear())
+t2m_img = ax.pcolorfast(lons, lats, t2m.data*800+wnf.data,
+                        cmap='RdYlBu_r',
+                        alpha=0.8,
+                        zorder=100)
+
+# PRMSL contours
+prmsl_pc=plot_cube(0.25,-180,180,
+                         -90,90)   
+prmsl = prmsl.regrid(prmsl_pc,iris.analysis.Linear())
+lats = prmsl.coord('latitude').points
+lons = prmsl.coord('longitude').points
+lons,lats = numpy.meshgrid(lons,lats)
+CS=ax.contour(lons, lats, prmsl.data*0.01,
+                           colors='black',
+                           linewidths=1.0,
+                           alpha=1.0,
+                           levels=numpy.arange(870,1050,10),
+                           zorder=200)
+
+# Overlay the latent-space representation in the SE Pacific
+ax2=fig.add_axes([0.025,0.05,0.15,0.15*16/9])
+ax2.set_xlim(0,10)
+ax2.set_ylim(0,10)
+ax2.set_axis_off() # Don't want surrounding x and y axis
+x=numpy.linspace(0,10,10)
+latent_img = ax2.pcolorfast(x,x,ls.numpy().reshape(10,10),
+                           cmap='viridis',
+                             alpha=1.0,
+                             vmin=-3,
+                             vmax=3,
+                             zorder=1000)
+
+# Render the figure as a png
+fig.savefig('random_ls.png')
