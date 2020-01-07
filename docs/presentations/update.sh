c028161abb5f25f599a6a0b0101aae7ab2e8c0a9
Philip Brohan <philip@brohan.org>
2020-01-07 17:14:29 +0000

    Added the oral presentation slide deck to the docs.

--- docs/presentations/update.sh
+++ docs/presentations/update.sh
@@ -4,4 +4,6 @@
 
 ./sync.py --bucket=philip.brohan.org.big-files --prefix=ML_GCM --name=ML_GCM_poster.pdf
 
+./sync.py --bucket=philip.brohan.org.big-files --prefix=ML_GCM --name=MetO_ML_20191126.key
+
 convert -geometry 1080x764 ML_GCM_poster.pdf ML_GCM_poster.png
