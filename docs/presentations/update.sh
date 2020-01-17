82336b3d838392487a965120323299b511383c8c
Philip Brohan <philip@brohan.org>
2020-01-17 09:25:09 +0000

    Added slide deck in PPT format.

--- docs/presentations/update.sh
+++ docs/presentations/update.sh
@@ -6,4 +6,6 @@
 
 ./sync.py --bucket=philip.brohan.org.big-files --prefix=ML_GCM --name=MetO_ML_20191126.key
 
+./sync.py --bucket=philip.brohan.org.big-files --prefix=ML_GCM --name=MetO_ML_20191126.pptx
+
 convert -geometry 1080x764 ML_GCM_poster.pdf ML_GCM_poster.png
